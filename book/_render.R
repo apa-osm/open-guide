991ca42dfa68f7d2800ce2ac81ac72a74dcfff79
debruine <debruine@gmail.com>
2022-06-14 16:02:12 +0100

    updates

--- book/_render.R
+++ book/_render.R
@@ -2,7 +2,7 @@
 xfun::in_dir("book", bookdown::render_book("index.Rmd", "bookdown::bs4_book"))
 browseURL("docs/index.html")
 
-# copy data directory to docs
+# copy image directory to docs
 R.utils::copyDirectory(
   from = "book/images",
   to = "docs/images",
